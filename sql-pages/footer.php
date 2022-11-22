6c31d339f6a9bba2ab4c89d16f5f3ce236fd2486
HardikShah563 <hardikts@gmail.com>
2022-11-23 01:24:58 +0530

    index.php done

---
+++ sql-pages/footer.php
@@ -0,0 +1,9 @@
+<!-- ---- Footer Section ---- -->
+<section class="footer" id="foother">
+    <footer>
+        <hr>
+        <br>
+        <p>Build with <i class="fa-solid fa-heart" id="heart"></i></p>
+        <p id="copyrights">&copy; Copyright beverrinks | All Rights Reserved</p>
+    </footer>
+</section>
